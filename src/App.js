ecb1657932f188a862c5e8d570b9545aae213ec0
kauan nery <113049473+Kauannery@users.noreply.github.com>
2023-06-09 09:07:20 -0300

    REACT.JS
    REACT

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, {useState,useEffect} from 'react';
+
+import './App.css';
+
+function App() {
+   const [hora,setHora] = useState(4);
+   const [minuto,setMinuto] = useState(30);
+   const [segundo,setSegundo] = useState(0);
+
+   useEffect(()  => {
+
+          const interval = setInterval(() => {
+
+              setSegundo(segundo+1);
+              if(segundo == 59){
+                  setSegundo(0);
+                  setMinuto(minuto+1);
+                  if(minuto == 60){
+                      setMinuto(0);
+                      setHora(hora+1);
+                  }                             
+                 }
+
+
+          }, 1000);
+
+          return () => clearInterval(interval);
+
+      
+   });
+
+
+
+  return (
+    <div>
+      <h2>{hora}:{minuto}:{segundo}</h2>
+    </div>
+  );
+}
+
+export default App;
